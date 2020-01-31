a0137ba282d9532a7235a9bebc393efda69043ce
vinay.s.bellamkonda@gmail.com <vinay.s.bellamkonda@gmail.com>
2020-01-31 09:14:08 +0530

    added Login_v4 folder

---
+++ Login_v4/Login_v4/popup.js
@@ -0,0 +1,51 @@
+/*function getCookies(domain, name, callback) {
+  chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
+      if(callback) {
+          callback(cookie.value);
+      }
+  });
+}
+
+//usage:
+window.onload=function(){
+getCookies("http://127.0.0.1", "email", function(email) {
+  alert(email);
+});
+}
+*/
+var ID='abcd';
+/*setTimeout(function(){
+  window.location.reload(1);
+}, 1000);*/
+
+window.onload=function(){
+  getCookies("http://127.0.0.1", "login");
+}
+    function getCookies(domain, name) 
+    {
+        chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
+            ID = cookie.value;
+            console.log(cookie.value)
+            
+           
+            checkId();
+        });
+    }
+
+    function checkId() {
+      //alert(ID)
+      if (ID == 'true') {
+        //console.log("hai");
+        document.getElementById('login').style.visibility = 'hidden';
+        document.getElementById('register').style.visibility = 'hidden';
+    } else if(ID=='false'){
+      document.getElementById('logout').style.visibility = 'hidden';
+      document.getElementById('register').style.visibility = 'hidden';
+    }
+    else if(ID=='abcd'){
+      document.getElementById('logout').style.visibility = 'hidden';
+    }
+    }
+    
+
+     
